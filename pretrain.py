1d0d604b9b74f85e22970b961be462b482a38c5b
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-04 22:29:02 +0800

    Update pretrain.py

--- pretrain.py
+++ pretrain.py
@@ -1,7 +1,35 @@
 import json
 from gensim.models import FastText
 
-def loadData(fileName):
+def loadData(fileName, fields):
+	
 	with open(fileName, 'r') as f:
-		data = f.readlines()
+		dataset = f.readlines()
+		
+	corpus = []
+	for i in range(len(dataset)):
+		datum = json.loads(dataset[i])
+		for field in fields:
+			corpus.append(datum[field])
+	return corpus
+
+def clean(text):
+	text = str(text)
+	text = text.lower()
+	text = re.sub(r'\'s',r'\tis',text)
+	text = re.sub(r'\'ll',r'\twill',text)
+	text = re.sub(r'\'m',r'\tam',text)
+	text = re.sub(r'\'re',r'\tare',text)
+	text = re.sub(r'\'d',r'\twould',text)
+	text = re.sub(r'n\'t',r'\tnot',text)
+	text = re.sub('[^a-zA-Z0-9]',' ',text) 
+	text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+	return text
+
+def wv(*datasets, clean = False, trim = False, spacy = False):
+	data = []
+	for (fileName, fields) in datasets:
+		data += loadData(fileName, fields)
+	if clean:
+		data = [clean(datum) for datum in data]
 	return data
